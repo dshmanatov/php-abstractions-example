00d05d1def2e540a363512d6939b70f5ace01b80
Дмитрий Шманатов <dshmanatov@gmail.com>
2021-04-04 12:37:04 +0400

    Made Resource stockable

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -2,21 +2,28 @@
 
 namespace App\Models;
 
+use App\Contracts\Stockable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Resource
  *
- * @property int    $id
- * @property string $name
+ * @property int     $id
+ * @property string  $name
+ * @property integer $stock
  * @package App\Models
  */
-class Resource extends Model
+class Resource extends Model implements Stockable
 {
     use HasFactory;
 
     protected $fillable = ['name', 'stock',];
 
     public $timestamps = false;
+
+    public function getStock()
+    {
+        return $this->stock;
+    }
 }
